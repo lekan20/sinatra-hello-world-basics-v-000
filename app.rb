26c39124a52009dbbfe3bbf2cde7a50bccf8a6be
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-16 11:13:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  post '/' do
+    # writes the status
+    resp.status = 200
+  end
 
+  get '/' do
+    # writes the string that shows on the host
+    "Hello, World!"
+  end
 
 end
